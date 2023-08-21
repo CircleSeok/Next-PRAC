ddb5c72a71ebdbee5b1bfc7308ab0d89635bbc83
Wonseok <dnjstjr970302@gamil.com>
2023-08-21 21:05:46 +0900

    라우터 세팅

---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+function EventDetailPage() {
+  return (
+    <div>
+      <h1>이벤트 상세</h1>
+    </div>
+  );
+}
+
+export default EventDetailPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function AllEventPage() {
+  return (
+    <div>
+      <h1>모든 이벤트</h1>
+    </div>
+  );
+}
+
+export default AllEventPage;
